748d370d0da82a2bba0e264eb1978790d4cc7891
Bari Jonathan <bari.jonathan@live.fr>
2019-11-21 16:28:30 +0100

    - Nickname is now stored in local storage - If the user doesn't have a nickname a random on in generated until the first rename - Adding various function (generateName(), getName(), getNow()) - Adding several comments and better separation of the code for optimization and easier reading - Adding the highlight and think feature
    - Overall rework of t he ChatMessage.js file (External filters for a more simple and maintanable template)

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <header></header>
         <Chat />
       </div>
     )
